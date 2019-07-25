79f98fa829dd2b591496ae93fb507a0abc307fb3
NavyaaSharma <navyaasharma700@rediffmail.com>
2019-07-25 22:44:19 +0530

    deployment

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
-var keys={
-    FacebookAppID:'2352343935041390',
-    FacebookAppSecret:'0726add3156d0dd27032a53ed7cef6ff',
-    GoogleClientID:'128768903231-op9cdnl0lr2c3aqj5m4vfmtv30gqdfvt.apps.googleusercontent.com',
-    GoogleClientSecret:'zu3IqxhebCKfZK7Uy5s8iHwQ'
+if(process.env.NODE_ENV ==='production')
+{
+    module.exports=require('./prod')
 }
-
-module.exports=keys
+else
+{
+    module.exports=require('./dev')
+}
---
+++ config/prod.js
@@ -0,0 +1,8 @@
+var prod={
+    FacebookAppID:'2352343935041390',
+    FacebookAppSecret:'0726add3156d0dd27032a53ed7cef6ff',
+    GoogleClientID:'128768903231-op9cdnl0lr2c3aqj5m4vfmtv30gqdfvt.apps.googleusercontent.com',
+    GoogleClientSecret:'zu3IqxhebCKfZK7Uy5s8iHwQ'
+}
+
+module.exports=prod
