f673a31f983a425a9158dffcca3c8a4afa2e12b6
NavyaaSharma <navyaasharma700@rediffmail.com>
2019-07-28 10:38:41 +0530

    fb for prod

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 var prod={
     MongoDB:process.env.MONGO_DB,
-    FacebookAppID:'2352343935041390',
-    FacebookAppSecret:'0726add3156d0dd27032a53ed7cef6ff',
+    FacebookAppID:process.env.FB_APP_ID,
+    FacebookAppSecret:process.env.FB_APP_SECRET,
     GoogleClientID:'128768903231-op9cdnl0lr2c3aqj5m4vfmtv30gqdfvt.apps.googleusercontent.com',
     GoogleClientSecret:'zu3IqxhebCKfZK7Uy5s8iHwQ'
 }
